8220b6accda89b4dd3224012c8a3418f999c9523
Phathumzi <2129411@students.wits.ac.za>
2023-03-22 11:30:14 +0200

    first commit

---
+++ homePage.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Home Page</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+
+<body>
+
+    <div class="main">
+        <div class="navbar">
+            <div class="icon">
+                <h5 class="logo">shift</h5>
+            </div>
+
+            <div class="menu">
+                <ul>
+                    <li><a href="/watch.php">WATCH</a></li>
+                    <li><a href="/play.php">PLAY NOW!</a></li>
+                    <li><a href="/Account.php">ACCOUNT</a></li>
+
+                </ul>
+            </div>
+
+            <div class="search">
+                <input class="srch" type="search" name="" placeholder="search">
+                <a href="#"> <button class="btn">Search</button></a>
+            </div>
+
+        </div>
+        <div class="content">
+            <h1>WELCOME <br><span>TO</span> <br>SHIFT</h1>
+            <p class="par">***
+                <br> ***
+            </p>
+
+            <button class="cn"><a href="#">REFRESH</a></button>
+
+
+        </div>
+    </div>
+    </div>
+    </div>
+    <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+
+    <?php
+
+
+
+
+    ?>
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>
---
+++ play.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/app.css" />
+    <title>PLAY NOW PAGE</title>
+</head>
+
+<body>
+    <h1> AVAILABLE GAMES</h1>
+
+    <div class="menu">
+        <ul>
+            <li><a href="/connect4.php">Connect4</a></li>
+
+
+        </ul>
+    </div>
+
+
+
+
+</body>
+
+</html>
